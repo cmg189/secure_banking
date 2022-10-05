22389d89bb9157d53570a77d52f44c455ca96533
cmg189 <tx189@yahoo.com>
2022-10-05 18:51:39 -0500

    contains only function prototypes

--- class.h
+++ class.h
@@ -1,14 +1,8 @@
-class bank_subject{
-	public:
-		bank_subject(string name);
-		void
-	private:
-		string name;
-		float balance;
-}
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <cstring>
+using namespace std;
 
-bank_subject:: bank_subject(string name){
-	this->name = name;
-	this->balance = 0.0;
-	return;
-}
+class bank_subject;
