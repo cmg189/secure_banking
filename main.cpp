23264b8760cd8ed96c9c4f58859a4f2b63962ce2
cmg189 <tx189@yahoo.com>
2022-10-05 18:53:38 -0500

    calculated number of instructions

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include "headers.h"
+#include "class.h"
 
 int main(int argc, char *argv[]){
 
-	// ensure program is executed correctly
+	// ensure program is executed correctly and file exists
 	check_exe(argc, argv);
 
+	int num_instructions = count_instructions(argv);
 
 	cout << "\nProgram ended\n\n";
 	return 0;
