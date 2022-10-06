035be502f1d9a4c9b276808ad238ea0cdb6c1782
cmg189 <tx189@yahoo.com>
2022-10-05 23:55:04 -0500

    created objects for each instruction

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include "headers.h"
-#include "class.h"
 
 int main(int argc, char *argv[]){
 
@@ -12,9 +11,10 @@ int main(int argc, char *argv[]){
 	// get each line from file
 	vector<string> instructions = get_instructions(num_instructions, argv);
 
-	// get each word from instruction
-	vector<string> command = get_instruction_words(instructions);
+	// create objects for each instruction
+	vector<struct instruction> instruction_objects = make_objects(instructions, num_instructions);
 
+	
 
 	cout << "\nProgram ended\n\n";
 	return 0;
