dd659a40a94d7714fd90c98c158287689b502979
cmg189 <tx189@yahoo.com>
2022-10-07 16:11:44 -0500

    added private member variables to reference_monitor

--- class.h
+++ class.h
@@ -6,47 +6,52 @@
 #include <utility>
 using namespace std;
 
-class bank_subject{
-	public:
-		bank_subject(string name);
-
-	private:
-		string name;
-		float balance;
-};
-
-
-class bank_object{
-	public:
-		bank_object(string name);
-
-	private:
-		string name;
-		float balance;
-};
-
 struct instruction{
 	string command;
 	string subject_name;
 	string object_name;
+	string level;
 	string amount;
 	bool correct_format;
 };
 
-class reference_monitor{
+class Bank_subject{
 	public:
-		reference_monitor();
+		Bank_subject(string name);
+		string get_name();
+		float get_balance();
+		void set_balance(float amount);
+	private:
+		string name;
+		float balance;
+};
 
-		void add_subject(class bank_subject, string level);
-		void add_object(class bank_object, string level);
+class Bank_object{
+	public:
+		Bank_object(string name);
+
+		string get_name();
+		float get_balance();
+		void set_balance(float amount);
+	private:
+		string name;
+		float balance;
+};
+
+
+class Reference_monitor{
+	public:
+		Reference_monitor();
+
+		void add_subject(pair<class Bank_subject, string> person);
+		void add_object(pair<class Bank_object, string> account);
 
 		void exe_query();
 
 
 	private:
-		pair<class bank_subject, string> subject_info;
-		pair<class bank_object, string> object_info;
-		vector< pair<class bank_subject, string> > subject_collection;
-		vector< pair<class bank_object, string> > object_collection;
+
+		vector< pair<class Bank_subject, string> > subject_collection;
+		vector< pair<class Bank_object, string> > object_collection;
 
 };
