a41b9ce3691ce9866c798f128d087816164995a7
cmg189 <tx189@yahoo.com>
2022-10-07 16:13:25 -0500

    initalizing reference_monitor object

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include "headers.h"
 
+enum Security_levels{ LOW, MEDIUM, HIGH };
+const char* levels[] = {"LOW", "MEDIUM", "HIGH"};
+
 int main(int argc, char *argv[]){
 
 	// ensure program is executed correctly and file exists
@@ -14,10 +17,11 @@ int main(int argc, char *argv[]){
 	// create objects for each instruction and check format
 	vector<struct instruction> instruction_objects = make_objects(instructions, num_instructions);
 
-	vector<Bank_subject> subject_collection;
-	vector<Bank_object> object_collection;
+	// collection of subjects and objects
+	Reference_monitor monitor();
 
-	// create collection of subjects and objects
+
+	// add subjects and objects to reference monitor
 	for(int i=0; i< num_instructions; i++){
 
 		// convert string to char array for strcmp() to work
@@ -26,15 +30,20 @@ int main(int argc, char *argv[]){
 
 		// add new subject
 		if( strcmp("ADDSUB", command) == 0 ){
-			Bank_subject new_subject(instruction_objects[i].subject_name);
-			subject_collection.push_back(new_subject);
+
+			pair<class Bank_subject, string> person;
+			person.first = instruction_objects[i].subject_name;
+			person.second = instruction_objects[i].subject_name;
 
 		// add new object
 		}else if ( strcmp("ADDOBJ", command) == 0){
-			Bank_object new_object(instruction_objects[i].object_name);
-			object_collection.push_back(new_object);
+
+			pair<class Bank_object, string> account;
+			account.first = instruction_objects[i].object_name;
+			account.second = instruction_objects[i].object_name;
 		}
 	}
+
 
 
 	cout << "\nProgram ended\n\n";
