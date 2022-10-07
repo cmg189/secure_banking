6a257ad4ac9a1bae044a17b52966360ca5b16403
cmg189 <tx189@yahoo.com>
2022-10-07 16:10:32 -0500

    added mutators for Reference_monitor

--- class.cpp
+++ class.cpp
@@ -24,3 +24,14 @@ Bank_object:: Bank_object(string name){
 string Bank_object:: get_name(){ return name; }
 float Bank_object:: get_balance(){ return balance; }
 void Bank_object:: set_balance(float amount){ this->balance = amount; }
+
+void Reference_monitor:: add_subject(pair<class Bank_subject, string> person){
+	this->subject_collection.push_back(person);
+	return;
+}
+
+void Reference_monitor:: add_object(pair<class Bank_object, string> account){
+	this->object_collection.push_back(account);
+
+	return;
+}
