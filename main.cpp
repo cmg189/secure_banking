1daab67df1c41793169a25573cfbfb2f4947ba53
cmg189 <tx189@yahoo.com>
2022-10-07 23:05:37 -0500

    print_status() added

--- main.cpp
+++ main.cpp
@@ -27,6 +27,21 @@ int main(int argc, char *argv[]){
 	// initalize collection of subjects and objects
 	create_collection(monitor, instruction_objects, num_instructions);
 
+	// output all information
+	monitor.print_status(instruction_objects);
+
+	/*
+	vector< pair<string, string> > temp_subjects = monitor.get_subjects();
+	vector< pair<string, string> > temp_objects = monitor.get_objects();
+	for(int i=0; i< temp_subjects.size(); i++){
+		pair<string, string> temp_person = temp_subjects.at(i);
+		cout << temp_person.first << " has " << temp_person.second << endl;
+	}
+	for(int i=0; i< temp_objects.size(); i++){
+		pair<string, string> temp_account = temp_objects.at(i);
+		cout << temp_account.first << " has " << temp_account.second << endl;
+	}
+	*/
 	cout << "\nProgram ended\n\n";
 	return 0;
 }
